be1110ef8c6e773204651bd72b259b7f719b2719
Sujal Uprety <70135815+SujalUprety@users.noreply.github.com>
2022-07-18 11:18:36 +0545

    new version

--- Build C/build.c
+++ Build C/build.c
@@ -6,13 +6,48 @@
 char* GetExtension(char*); // to get the extension of the filename
 char* GetFilename(char*);   //to get the filename without the extension of the file
 
-int main() {
+void BuildAllProgram(); //to build all the c programs in a directory
+void BuildOneProgram(); //to build only one file with only one arguement in command with C file name
+
+void BuildFile(char*, char*); //to build the file one by one by calling this function
+
+void Help();
+
+int main(int argc, char **argv) {
+
+    if(argc > 1) {
+
+        if(argv[1][0] == '-') {
+            if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1],"-h") == 0) 
+                Help();
+            else
+                printf("Invalid Option\n");
+
+            return 0;
+        }
+
+        for(int i = 1; i < argc; i++) {
+            if(argv[i+1] != NULL){    
+                if(strcmp(GetExtension(argv[i+1]), ".c") != 0) {
+                    BuildFile(argv[i-1], argv[++i]);
+                    continue;
+                }
+            }
+            BuildOneProgram(argv[i]);
+        }
+    }
+    else 
+        BuildAllProgram();
+
+    return 0;
+}
+
+void BuildAllProgram() {
 
     DIR *d;
     struct dirent *dir;
 
     char ext[50], filename[50];
-    char command[50];
 
     d = opendir(".");
 
@@ -43,30 +78,62 @@ int main() {
                 if(strcmp(dir->d_name, "build.c") == 0) continue;
 
                 strcpy(filename, GetFilename(dir->d_name));
-
-                /*
-                    Printing the details of building in termainal
-                    to make sure everything is working perfectly
-                */
-                printf("Building %s to %s\n", dir->d_name, filename);
-                
-                /*
-                    storing in string variable command the command 
-                    which is used to build c program by buffering the values.
-                */
-                snprintf(command, sizeof(command), "gcc %s -o %s", dir->d_name, filename);
-
-                /*
-                    using the command stored above just to run the command in 
-                    opened terminal. For now, using bash terminal.
-                */
-                system(command);
+                BuildFile(dir->d_name, filename);
             }
         }
         closedir(d);
     }
+    
+}
 
-    return 0;
+void BuildOneProgram(char* programname) {
+    char ext[50], filename[50];
+
+    strcpy(ext, GetExtension(programname));
+
+    if(strcmp(ext, ".c") != 0) {
+        printf("Not a C program file\n");
+        return;
+    }
+
+    strcpy(filename, GetFilename(programname));
+    BuildFile(programname, filename);
+    
+}
+
+void BuildFile(char* programname, char* filename) {
+
+    char command[50];
+
+
+    /*
+        Printing the details of building in termainal
+        to make sure everything is working perfectly
+    */
+    printf("Building %s to %s\n", programname, filename);
+
+    /*
+        storing in string variable command the command 
+        which is used to build c program by buffering the values.
+    */
+    snprintf(command, sizeof(command), "gcc %s -o %s", programname, filename);
+
+    /*
+        using the command stored above just to run the command in 
+        opened terminal. For now, using bash terminal.
+    */
+    system(command);
+}
+
+void Help() {
+    printf("Usage: build [Option]... [Filename of c program]...[Filename to build program]...\n");
+    printf("Example: build test.c testing hello.c hola hi.c\n");
+    printf("Options:\n--help\t-h\tPrintUsage\n");
+    printf("Using command only build will build all the c programs files stored in that current directory\n");
+    printf("Example: build\n");
+    printf("Can build multiple c programs at once and if the filename in which the build should be saved is not given then it will be saved as the same name as the c program without extension\n");
+    printf("Example: build test.c testing hello.c hi.c hola\n");
+    printf("This command will build test.c to testing, hello.c to hello because no filename is given for this and hi.c to hola\n");
 }
 
 char* GetExtension(char* name) {
